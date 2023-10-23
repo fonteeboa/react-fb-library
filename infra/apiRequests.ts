08c09a7f87b300f6246610ebc4da80dc64cc4597
João Victor <69174195+galvao845@users.noreply.github.com>
2023-10-23 16:20:43 -0300

    doc: adicionando comentarios as rotinas

--- infra/apiRequests.ts
+++ infra/apiRequests.ts
@@ -4,6 +4,16 @@ import { baseConfig } from "./constantsRequets";
 
 let config = baseConfig;
 
+/**
+ * Retrieves a service from the specified URL with optional authentication.
+ *
+ * @param {ServiceParams} params - The parameters for retrieving the service.
+ * @param {string} params.baseUrl - The base URL of the service.
+ * @param {string} [params.route=''] - The route of the service.
+ * @param {object} [params.body={}] - The body of the request.
+ * @param {boolean} [params.authToken=false] - Whether authentication is required.
+ * @returns {Promise<T>} A promise that resolves with the service data.
+ */
 export const getService = async <T>(params: ServiceParams): Promise<T> => {
   const { baseUrl, route = '', body = {}, authToken = false } = params;
   if (authToken) {
@@ -18,6 +28,16 @@ export const getService = async <T>(params: ServiceParams): Promise<T> => {
   });
 };
 
+/**
+ * Sends a POST request to the specified URL with the given request body.
+ *
+ * @param {ServiceParams} params - The parameters for the service request.
+ * @param {string} params.baseUrl - The base URL for the API.
+ * @param {string} [params.route] - The route for the API.
+ * @param {object} [params.body] - The request body.
+ * @param {boolean} [params.authToken=false] - Whether to include an authentication token.
+ * @return {Promise<T | boolean>} - A promise that resolves to the response data or false if there's an error.
+ */
 export const postService = async <T> (params: ServiceParams): Promise<T | boolean> => {
     const { baseUrl, route = '', body = {}, authToken = false } = params;
   if (authToken) {
@@ -33,6 +53,12 @@ export const postService = async <T> (params: ServiceParams): Promise<T | boolea
 };
 
 
+/**
+ * Sends a PUT request to the specified URL with the provided body and returns the response data.
+ *
+ * @param {ServiceParams} params - The parameters for the service request.
+ * @returns {Promise<T | boolean>} The response data or false if an error occurs.
+ */
 export const putService = async <T>(params: ServiceParams): Promise<T | boolean> => {
   const { baseUrl, route = '', body = {}, authToken = false } = params;
   if (authToken) {
@@ -48,6 +74,15 @@ export const putService = async <T>(params: ServiceParams): Promise<T | boolean>
 };
 
 
+/**
+ * Deletes a service.
+ *
+ * @param {ServiceParams} params - The parameters for deleting the service.
+ * @param {string} params.baseUrl - The base URL for the service.
+ * @param {string} params.route - The route for the service.
+ * @param {boolean} params.authToken - Whether an authentication token is required.
+ * @return {Promise<T | boolean>} A promise that resolves to the deleted data or a boolean indicating success.
+ */
 export const deleteService = async <T>(params: ServiceParams): Promise<T | boolean> => {
   const { baseUrl = '', route = '', authToken = false } = params;
   if (authToken) {
@@ -62,6 +97,12 @@ export const deleteService = async <T>(params: ServiceParams): Promise<T | boole
   });
 }
 
+/**
+ * Checks if a GET request is successful.
+ *
+ * @param {ServiceParams} params - The parameters for the GET request.
+ * @return {Promise<boolean>} - Returns a boolean indicating if the GET request was successful.
+ */
 export const checkGetRequest = async (params: ServiceParams): Promise<boolean> => {
   const { baseUrl, route = '' } = params;
   try {
@@ -73,6 +114,12 @@ export const checkGetRequest = async (params: ServiceParams): Promise<boolean> =
   }
 };
 
+/**
+ * Sets the token authentication for the given request configuration.
+ *
+ * @param {RequestConfig} config - The request configuration to set the authentication token for.
+ * @return {Promise<RequestConfig>} The modified request configuration with the authentication token set, if available.
+ */
 const setTokenAuth = async (config: RequestConfig): Promise<RequestConfig> => {
   const authToken = await sessionStorage.getItem('authToken');
   if (authToken) {
