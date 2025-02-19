ba5d7d6540478640351df5491491fe3ba70c480b
João Victor <galvao845@gmail.com>
2025-02-18 22:32:00 -0300

    menu: melhorando geracao de menu key

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -4,35 +4,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MenuItem, SubMenuItem, SidebarMenuProps } from './types';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
+const regex = /\s+/g;
 const { SubMenu } = Menu;
 
-/**
- * Renders a sub-menu with the provided sub-menu items.
- *
- * @param {SubMenuItem[]} subMenu - An array of sub-menu items.
- * @param {any} t - A translation function.
- * @return {ReactNode[]} - An array of React elements representing the rendered sub-menu.
- */
-function renderSubMenu(subMenu: SubMenuItem[]) {
+function generateKey(prefix: string, index: number, label: string): string {
+   const sanitizedLabel = label.replace(regex, '-').toLowerCase();
+   const timestamp = Date.now().toString(36);
+   const randomString = Math.random().toString(36).substring(2, 10);
+   return `${prefix}-${index}-${sanitizedLabel}-${timestamp}-${randomString}`;
+}
+
+function renderSubMenu(subMenu: SubMenuItem[], parentKey: string) {
    return subMenu.map((item, index) => {
       const { label, icon, subMenu: subSubMenu, route, external, linkComponent } = item;
       const itemIcon = icon ? ( typeof icon === 'string' ? <i className={icon}></i> : <FontAwesomeIcon icon={icon as IconProp} /> ) : null;
+      const itemKey = generateKey(parentKey, index, label);
 
       if (subSubMenu && subSubMenu.length > 0) {
          return (
-            <SubMenu key={'subMenu2'+ index}  title={label} icon={itemIcon} data-testid={index + '-' + label}>
-               {renderSubMenu(subSubMenu)}
+            <SubMenu key={itemKey} title={label} icon={itemIcon} data-testid={itemKey}>
+               {renderSubMenu(subSubMenu, itemKey)}
             </SubMenu>
          );
       } else if (route) {
          return (
-            <Menu.Item key={'MenuItem'+ index}  icon={itemIcon} data-testid={index + '-' + label}>
+            <Menu.Item key={itemKey} icon={itemIcon} data-testid={itemKey}>
                {external ? <a href={route} target="_blank" rel="noopener noreferrer"> { label } </a> : linkComponent}
             </Menu.Item>
          );
       } else {
          return (
-            <Menu.Item key={'MenuItem'+index}  icon={itemIcon} data-testid={index + '-' + label}>
+            <Menu.Item key={itemKey} icon={itemIcon} data-testid={itemKey}>
                {label}
             </Menu.Item>
          );
@@ -40,13 +42,14 @@ function renderSubMenu(subMenu: SubMenuItem[]) {
    });
 }
 
-
 function renderMenuItem(item: MenuItem, index: number) {
+   const itemKey = generateKey('MainMenu', index, item.label);
+
    if (item.subMenu && item.subMenu.length > 0) {
       return (
          <SubMenu
-            key={'SubMenu'+index}
-            data-testid={index + '-' + item.label}
+            key={itemKey}
+            data-testid={itemKey}
             title={item.label}
             icon={
                item.icon ? (
@@ -58,24 +61,18 @@ function renderMenuItem(item: MenuItem, index: number) {
                ) : null
             }
          >
-            {renderSubMenu(item.subMenu)}
+            {renderSubMenu(item.subMenu, itemKey)}
          </SubMenu>
       );
    } else {
       return (
-         <Menu.Item key={'Menu'+index} icon={item.icon ? <FontAwesomeIcon icon={item.icon as IconProp} /> : null} data-testid={index + '-' + item.label}>
+         <Menu.Item key={itemKey} icon={item.icon ? <FontAwesomeIcon icon={item.icon as IconProp} /> : null} data-testid={itemKey}>
             {item.label}
          </Menu.Item>
       );
    }
 }
 
-/**
- * Renders the sidebar menu component.
- *
- * @param {SidebarMenuProps} menuItems - The menu items to be displayed in the sidebar.
- * @return {ReactElement} The rendered sidebar menu component.
- */
 const Sidebar: React.FC<SidebarMenuProps> = ({ menuItems }) => {
    return (
       <Menu
